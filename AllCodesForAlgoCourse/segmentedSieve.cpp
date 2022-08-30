2fb93aaf6ad85afa7a350e43f7d33c3d1a95ae7f
Md Sadman Hafiz <hafiz.sust333@gmail.com>
2022-08-30 16:25:54 +0600

    Create segmentedSieve.cpp

---
+++ AllCodesForAlgoCourse/segmentedSieve.cpp
@@ -0,0 +1,29 @@
+typedef long long ll;
+vector < int > primes; /*এখানে primes ভেক্টরে sieve এর মাধ্যমে প্রাইম নাম্বারগুলো জেনারেট করে নেওয়া হয়েছে  */
+void segmentedSieve(ll L, ll R)
+{
+ bool isPrime[R-L+1];
+ for(int i=0 ; i<=R-L+1 ; i++)
+    isPrime[i]=true;
+    
+ if(L==1)
+    isPrime[0]=false;
+    
+ for(int i=0 ; primes[i]*primes[i]<=R ; i++)
+ {
+    ll curPrime=primes[i];
+    ll base=curPrime*curPrime;
+    if(base<L)
+    {
+       base=((L+curPrime-1)/curPrime)*curPrime;
+    }
+    for(ll j=base ; j<=R ; j+=curPrime)
+    isPrime[j-L]=false;
+ }
+ for(int i=0 ; i<=R-L ; i++)
+ {
+    if(isPrime[i]==true)
+        cout<<L+i<<endl;
+ }
+ cout<<endl;
+}
